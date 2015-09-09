091cdac8bd8d70631549fe42ff46b7a44eb1b617
zhangfushuai <1558391434@qq.com>
2015-09-09 15:58:05 +0800

    no message

---
+++ 学习/22.m
@@ -0,0 +1,21 @@
+8.15加班8点到6点  8小时
+9.1加班6点到早2点 8小时
+9.2加班6点到早6点 12小时
+9.5加班9点到6点   8小时
+
+NSArray *gids = [NSArray valueForKeyPath:@"gid"];
+//购物车清空
+NSString *sss = [gids componentsJoinedByString:@","];
+
+
+用的最多的 pod install ,pod update 每次都要升级cocoapods的spec仓库，在命令执行时添加参数可以略过此步。具体如下：
+pod install --verbose --no-repo-update
+pod update --verbose --no-repo-update
+
+
+secInfoLabel.lineBreakMode = NSLineBreakByWordWrapping;
+secInfoLabel.numberOfLines = 0;
+secInfoLabel.preferredMaxLayoutWidth = self.view.frame.size.width;
+preferredMaxLayoutWidth 这个属性就是与Autolayout相关的，必须设置了这个属性才能在Autolayout下让UILabel显示多行。
+
+
